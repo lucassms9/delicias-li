dc6eaef129bb512bc236a80a939c00ec1652e3af
Lucas Santos <lucassms9@hotmail.com>
2021-03-22 22:26:22 -0300

    ajustes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -22,8 +22,8 @@ const Mobile = () => {
                 <Col className="cl-item" span={32}>
                     <div className="content-item">
                         <div>
-                            <div>Solicitação de Pedidos: Terça à Quinta.</div>
-                            <div>Entrega de Pedidos: Sexta à Domingo.</div>
+                            <div>Solicitação de Pedidos: Segunda,Quarta e Sexta.</div>
+                            <div>Entrega de Pedidos: Terça, Quinta e Sábado.</div>
                         </div>
                     </div>
                 </Col>
@@ -83,8 +83,8 @@ const Web = () => {
                 <Col className="cl-item" span={8}>
                     <div className="content-item">
                         <div>
-                            <div>Solicitação de Pedidos: Terça à Quinta.</div>
-                            <div>Entrega de Pedidos: Sexta à Domingo.</div>
+                        <div>Solicitação de Pedidos: Segunda,Quarta e Sexta.</div>
+                            <div>Entrega de Pedidos: Terça, Quinta e Sábado.</div>
                         </div>
                     </div>
                 </Col>
@@ -99,8 +99,10 @@ const Web = () => {
                                     color: '#119af7',
                                     fontSize: 20,
                                 }}
+                                onClick={() =>  window.open(`https://www.facebook.com/Delicias-da-Li-107543851390857`, '_blank')}
                             />
                             <InstagramOutlined
+                            onClick={() =>  window.open(`https://www.instagram.com/elidi_deliciasdali/`, '_blank')}
                                 style={{
                                     marginLeft: 7,
                                     color: '#cf2c87',
@@ -114,8 +116,8 @@ const Web = () => {
                     <Divider className="divider" type="vertical" />
                     <div className="content-item">
                         <div>
-                            <div> Contato via Whats </div>
-                            <div className="whats-contact">
+                            <div style={{textAlign:'center'}}> Contato via Whats </div>
+                            <div onClick={() => window.open(`https://api.whatsapp.com/send/?phone=5515997623307&text=Olá, Gostaria de mais informações os sobre seu trabalho.&app_absent=0`, '_blank')} className="whats-contact">
                                 <div>
                                     <WhatsAppOutlined
                                         style={{
